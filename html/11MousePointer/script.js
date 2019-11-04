96b89e29f536efc53e310231b7912c90c9d0d65c
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-11-04 10:54:22 +0100

    11 klaar i think

--- html/11MousePointer/script.js
+++ html/11MousePointer/script.js
@@ -8,16 +8,26 @@ cvs.width = width;
 cvs.height = height;
 
 
+let mouse = {};
+let arrows = [];
 
-
+let Aerrow = new Arrow(new Vector2d(cvs.width/2,cvs.height/2),"hotpink",0);
+let F1 = new LinearFunction(0,0);
 
 function animate() {
     requestAnimationFrame(animate);
+    F1.defineLineWithTwoPoints();
     ctx.clearRect(0, 0, width, height);
+    let dx = mouse.x - Aerrow.pos.dx;
+    let dy = mouse.y - Aerrow.pos.dy;
+    Aerrow.angle = Math.atan2(dy, dx);
+    Aerrow.draw(ctx);
+
 }
 
-
-
-
-
 animate();
+
+addEventListener('mousemove',(evt)=>{
+mouse.x = evt.clientX;
+mouse.y = evt.clientY;
+});
