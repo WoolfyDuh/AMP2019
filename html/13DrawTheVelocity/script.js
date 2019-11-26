56a2c658dd5c4323b6611289cd7c3985cdbf4db2
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-11-26 11:44:47 +0100

    m

--- html/13DrawTheVelocity/script.js
+++ html/13DrawTheVelocity/script.js
@@ -9,13 +9,13 @@ canvas.width = width;
 canvas.height = height;
 
 
-let A = new DPoint(new Vector2d(0, 0), new Vector2d(3, 4), new Vector2d(0, 0), 20, "purple");
+let A = new DPoint(new Vector2d(1, 0), new Vector2d(3, 4), new Vector2d(0, 1), 20, "purple");
 
 function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, width, height);
     A.draw(ctx);
     A.update();
-    A.vel.draw(ctx, A.pos, "red", 5);
+    A.vel.draw(ctx, A.pos, "#00ffe4", 5);
 }
 animate();
