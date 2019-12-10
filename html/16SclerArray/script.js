a862f0da6a805f0c03cd8d27301861eefacc646a
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-12-10 12:39:06 +0100

    16 finished

--- html/16SclerArray/script.js
+++ html/16SclerArray/script.js
@@ -1,4 +1,3 @@
-
 const cvs = document.getElementById('canvas');
 const ctx = cvs.getContext('2d');
 
@@ -9,24 +8,26 @@ canvas.width = width;
 canvas.height = height;
 
 
-let C = new DPoint(new Vector2d(200,300),new Vector2d(0,0), new Vector2d(0,0),15,"Lime","C");
+let C = new DPoint(new Vector2d(200, 300), new Vector2d(0, 0), new Vector2d(0, 0), 15, "Lime", "C");
 let waypoints = [];
+let index = 0;
 
+for (let i = 0; i < 5; i++) {
+    let waypoint = new Point(new Vector2d(rng(cvs.width), rng(cvs.height)), 15, "white", "", true);
+    waypoints.push(waypoint);
+}
 
-function Animate(){
+function Animate() {
     requestAnimationFrame(Animate);
-    ctx.clearRect(0,0,cvs.width,cvs.height);
+    ctx.clearRect(0, 0, cvs.width, cvs.height);
     C.draw(ctx);
     C.pos.draw(ctx, new Vector2d(0, 0), 1, "white");
 
 
-    C.update();
-    C.vel.draw(ctx,C.pos,10, "White");
 
-    if(waypoints.length < 5){
-        let waypoint = new Point(new Vector2d(rng(cvs.width),rng(cvs.height)),15,"white","",true);
-        waypoints.push(waypoint);
-    }
+    C.vel.draw(ctx, C.pos, 10, "White");
+
+
     ctx.beginPath();
     ctx.strokeStyle = "AliceBlue";
     ctx.setLineDash([5, 15]);
@@ -37,35 +38,27 @@ function Animate(){
     ctx.closePath();
     ctx.stroke();
     ctx.setLineDash([0]);
-    for(let i =0; i < waypoints.length -1; i++){
+    for (let i = 0; i < waypoints.length - 1; i++) {
         waypoints[i].draw(ctx);
-        waypoints[i].label = i;
+        waypoints[i].label = i ;
     }
 
-    // if(C.vel.magnitude <= 0.3 ){
-    //     BCheck = !BCheck;
-    //     console.log("SWITCHI");
-    // }
-
-    // if  (BCheck){
-    // C.vel.differenceVector(B.position,C.pos);
-    // C.vel.scalMul(0.01);
-    // }
-    // if(!BCheck){
-    // C.vel.differenceVector(A.position,C.pos);
-    // C.vel.scalMul(0.01);
-    // }
-
-    if(C.vel.magnitude <=0.3){
-        C.vel.differenceVector(waypoints[i].position + 1,C.pos);
+    C.vel.differenceVector(waypoints[index].position, C.pos);
+    console.log(C.vel.magnitude);
+    if (C.vel.magnitude < 2) {
+        index += 1;
     }
-    C.vel.differenceVector(waypoints[0],C.pos);
+    if(index >= waypoints.length){
+        index = 0;
+    }
     C.vel.scalMul(0.01);
+
+    C.update();
 }
 
 Animate();
 
-function drawLineAB(){
+function drawLineAB() {
     ctx.beginPath();
     ctx.strokeStyle = "HotPink";
     ctx.setLineDash([5, 15]);
@@ -76,13 +69,13 @@ function drawLineAB(){
     ctx.setLineDash([0]);
 }
 
-function drawLineWaypoints(){
+function drawLineWaypoints() {
     ctx.beginPath();
     ctx.strokeStyle = "AliceBlue";
     ctx.setLineDash([5, 15]);
-    ctx.moveTo(waypoints[0].position.dx,waypoints[0].position.dy);
-    for(i=0; i < waypoints.length; i++){
-        ctx.lineTo(waypoints[i].position.dx,waypoints[i].position.dy);
+    ctx.moveTo(waypoints[0].position.dx, waypoints[0].position.dy);
+    for (i = 0; i < waypoints.length; i++) {
+        ctx.lineTo(waypoints[i].position.dx, waypoints[i].position.dy);
     }
     ctx.closePath();
     ctx.stroke();
