9b8af84025e3d326ffea527bb50cf67b9909f908
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-12-10 11:31:27 +0100

    finished 14 started 16

--- html/14BouncingBalls/script.js
+++ html/14BouncingBalls/script.js
@@ -8,15 +8,29 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let A = new DPoint(new Vector2d(200, 200), new Vector2d(5, 4), new Vector2d(0, 0), 15, "hotpink", "uwu")
+let wooo = [];
+let A = new DPoint(new Vector2d(200, 200), new Vector2d(5, 4), new Vector2d(0, 1), 15, "hotpink", "uwu")
 
 function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, width, height);
     A.update();
     A.draw(ctx)
+    A.vel.draw(ctx, A.pos, "#00ffe4", 5);
+    if(wooo.length < 100){
+        let wot = new DPoint(new Vector2d(rng(500), 200), new Vector2d(5, 4), new Vector2d(0, 1), 15, "hotpink");
+        wooo.push(wot);
+    }
+    for(let i = 0; i < wooo.length; i++){
+        wooo[i].draw(ctx);
+        wooo[i].update();
+        wooo[i].label = i + 1;
+        wooo[i].vel.draw(ctx, wooo[i].pos, "Lime", 5);
+    }
 
-    console.log(A.vel.angle())
 }
 
 animate();
+function rng(max) {
+    return Math.floor(Math.random() * max)
+}
