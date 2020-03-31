6f523895468005199b4dc4f65426db5cbe6513f7
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2020-03-31 19:30:42 +0200

    minor optimizations

--- html/21PinballBoogaloo/script.js
+++ html/21PinballBoogaloo/script.js
@@ -20,9 +20,7 @@ function Animate() {
     ctx.clearRect(0, 0, width, height);
     grid.map((ball) => {
         let distance = new Vector2d(1,1);
-        let dist = B.rad.magnitude;
         distance.differenceVector(ball.position,B.pos);
-        //distance.draw(ctx, B.pos, "honeydew", 1);
         if(distance.magnitude < B.radius + ball.radius){
             ball.color = "green";
             B.rad.dx = distance.dx;
@@ -30,7 +28,15 @@ function Animate() {
 
             B.tan.dx = -B.rad.dy;
             B.tan.dy = B.rad.dx;
-            setTimeout(flip, 50)
+
+            B.rad.normalized();
+            B.tan.normalized();
+
+            B.rad.magnitude = B.rad.dot(B.vel);
+            B.tan.magnitude = B.tan.dot(B.vel);
+
+            collide();
+        
         }
         else{
             ball.color = "white";
@@ -41,8 +47,6 @@ function Animate() {
 
 
     B.draw(ctx);
-    
-    
 
 }
 Animate();
@@ -52,8 +56,8 @@ function rng(max) {
     return Math.floor(Math.random() * max)
 }
 
-function flip(){
-    B.rad.magnitude = -B.rad.magnitude;
+function collide(){
+    B.rad.flipMagnitude();
     B.vel.sumVector(B.rad, B.tan);
 }
 
@@ -73,7 +77,7 @@ function PointArray(){
     for (let i = 0; i < amountOfPoints; i++) {
         let x = startColumnWidth + (i % numberOnRow) * columnWidth;
         let y = startRowHeight + (Math.floor(i / numberOnRow)) * rowHeight;
-        let A = new Point(new Vector2d(x, y), 30, "white", "A", false);
+        let A = new Point(new Vector2d(x, y), 30, "white", "", false);
         arr.push(A);
     }
     return arr;
