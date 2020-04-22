350b71319470d264890189e8a04002577a5da36a
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2020-04-22 11:30:10 +0200

    24 start

---
+++ html/24MassAndThings/script.js
@@ -0,0 +1,8 @@
+const cvs = document.getElementById('canvas');
+const ctx = cvs.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
