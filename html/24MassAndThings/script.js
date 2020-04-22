2c6591e74a19b1c45576885a228f73433021d01c
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2020-04-22 11:36:25 +0200

    2 balls

--- html/24MassAndThings/script.js
+++ html/24MassAndThings/script.js
@@ -6,3 +6,20 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+let A, B;
+
+A = new DPoint(new Vector2d(0,0), new Vector2d(1,2), new Vector2d(0,0), 10, "lime", "A");
+B = new DPoint(new Vector2d(500,500), new Vector2d(-1,-2), new Vector2d(0,0), 50, "hotpink", "B");
+
+function Animate(){
+    requestAnimationFrame(Animate);
+    ctx.clearRect(0,0, cvs.width, cvs.height);
+    A.update();
+    B.update();
+    
+    A.draw(ctx);
+    B.draw(ctx);
+}
+
+Animate();
