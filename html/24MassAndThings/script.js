8ee1c29022ea156a923cc96f387a31905439de3f
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2020-04-22 12:23:27 +0200

    added new vector2d function, bulk commit

--- html/24MassAndThings/script.js
+++ html/24MassAndThings/script.js
@@ -15,7 +15,16 @@ B = new DPoint(new Vector2d(500,500), new Vector2d(-1,-2), new Vector2d(0,0), 10
 A.mass = A.radius * A.radius;
 B.mass = B.radius * B.radius;
 
+A.rad = new Vector2d(1,1);
+B.rad = new Vector2d(1, 1);
+
+A.tan = new Vector2d(1, 1);
+B.tan = new Vector2d(1, 1);
+
 diff = new Vector2d(1,1);
+let Msum,MAB,MBA,P,Q,R,S;
+
+
 
 function Animate(){
     requestAnimationFrame(Animate);
@@ -23,15 +32,70 @@ function Animate(){
     A.update();
     B.update();
 
+
+    A.rad.differenceVector(B.pos, A.pos);
+    A.tan.perpendicular(B.rad);
+
+    A.rad.normalized();
+    A.tan.normalized();
+
+    A.rad.magnitude = B.rad.dot(B.vel);
+    A.tan.magnitude = B.tan.dot(B.vel);
+
+    B.rad.differenceVector(A.pos, B.pos);
+    B.tan.perpendicular(A.rad);
+
+    B.rad.normalized();
+    B.tan.normalized();
+
+    B.rad.magnitude = B.rad.dot(B.vel);
+    B.tan.magnitude = B.tan.dot(B.vel);
     diff.differenceVector(B.pos, A.pos);
     
     A.draw(ctx);
     B.draw(ctx);
-    
-    if(diff.magnitude < A.radius + B.radius)
-    diff.draw(ctx, A.pos, 10, "red");
+    A.vel.draw(ctx, A.pos, "hotpink", 40);
+    B.vel.draw(ctx, B.pos, "lime", 40);    
+
+    A.rad.draw(ctx, A.pos, "red", 40);
+    B.rad.draw(ctx, B.pos, "red", 40);   
+
+    A.tan.draw(ctx, A.pos, "green", 20);
+    B.tan.draw(ctx, B.pos, "green", 20);   
+
+    if(diff.magnitude < A.radius + B.radius){
+        Msum = A.mass + B.mass;
+        MAB = A.mass - B.mass;
+        MBA = B.mass - A.mass;
+        
+        P = new Vector2d(0,0);
+        Q = new Vector2d(0,0);
+        R = new Vector2d(0,0);
+        S = new Vector2d(0,0);
+
+        // P = MAB / Msum * A.rad;
+        // Q = 2 * B.mass / Msum * B.rad;
+        // R = 2 * A.mass / Msum * A.rad;
+        // S = MBA / Msum * B.rad
+        P.equals(A.rad);
+        Q.equals(B.rad);
+        R.equals(A.rad);
+        S.equals(B.rad);
+
+        P.scalMul(MAB / Msum);
+        Q.scalMul(2 * B.mass / Msum);
+        R.scalMul(2 * A.mass /Msum);
+        S.scalMul(MBA / Msum);
+
+        A.rad.sumVector(P, Q);
+        B.rad.sumVector(R, S);
+
+        A.vel.sumVector(A.rad, A.tan);
+        B.vel.sumVector(B.rad, B.tan);
+
+        }
     else{
-        diff.draw(ctx, A.pos, 10, "blue");   
+       // diff.draw(ctx, A.pos,"blue",10);   
     }
 }
 
